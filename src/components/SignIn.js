faaf46382ebf4f21df22be1556a9d1976e941369
hyounakura <leopardtender@gmail.com>
2021-01-18 19:04:34 +0900

    set name by button click

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -43,7 +43,6 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
-  console.log(disabled, string);
 
   useEffect(() => {
     const disabled = string === ''
@@ -70,12 +69,15 @@ export default function SignIn({ setName }) {
             onChange={(e) => setString(e.target.value)}
           />
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
             disabled={disabled}
+            onClick={() => {
+              setName(string);
+            }}
           >
             はじめる
           </Button>
